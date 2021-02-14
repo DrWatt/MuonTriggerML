93ed1dc66c13739dd9a7ae3cf42d7d2850b32684
DrWatt <marco.lorusso96@gmail.com>
2021-02-14 18:41:43 +0100

    Added small documentation

--- Q_Model/K_to_hls.py
+++ Q_Model/K_to_hls.py
@@ -7,91 +7,37 @@ from QKeras_Model import preproc
 import hls4ml
 from subprocess import check_output
 
-outrootname = "QKeras_Model_4"
+modelname = "QKeras_Model_60_50_30_40_15"
+outrootname ="QKeras_Model_60_50_30_40_15_HLS_noreuse"
 
+#Preprocessing data
 X_test,Y_test = preproc(True)
-# outputFile = root.TFile(outrootname + '/' + outrootname +"_hls.root","recreate")
-# c1 = root.TCanvas("c1","c1");
-name="RMSE"
-model = load_qmodel(outrootname + '/' + outrootname + '.h5')
 
-config = hls4ml.utils.config_from_keras_model(model, granularity='name')
-#print(config)
+#Loading QKeras model from disk
+model = load_qmodel(modelname + '/' + modelname + '.h5')
 
+#Creating configuration dictionary
+config = hls4ml.utils.config_from_keras_model(model, granularity='name',default_reuse_factor=1)
+
+#Activating tracing (i.e. saving also the results passed between hidden layers)
 for layer in config['LayerName'].keys():
     config['LayerName'][layer]['Trace'] = True
 
-#                   BIG NET
-# config['LayerName']['dense']['Precision']['weight'] = 'ap_fixed<31,9>'
-# config['LayerName']['dense']['Precision']['bias'] = 'ap_fixed<10,3>'
-# config['LayerName']['dense']['Precision']['result'] = 'ap_fixed<52,17>'
-# #config['LayerName']['dense_sigmoid']['Precision'] = 'ap_fixed<32,5>'
-# #config['LayerName']['dense_sigmoid']['table_size'] = '4096'
-# config['LayerName']['dense_sigmoid']['table_t'] = 'ap_fixed<52,17>'
-# config['LayerName']['dense_1']['Precision']['weight'] = 'ap_fixed<28,7>'
-# config['LayerName']['dense_1']['Precision']['bias'] = 'ap_fixed<11,4>'
-# config['LayerName']['dense_1']['Precision']['result'] = 'ap_fixed<49,18>'
-# #config['LayerName']['dense_1_sigmoid']['Precision'] = 'ap_fixed<32,5>'
-# #config['LayerName']['dense_1_sigmoid']['table_size'] = '4096'
-# config['LayerName']['dense_1_sigmoid']['table_t'] = 'ap_fixed<49,18>'
-# config['LayerName']['dense_2']['Precision']['weight'] = 'ap_fixed<11,3>'
-# config['LayerName']['dense_2']['Precision']['bias'] = 'ap_fixed<9,2>'
-# config['LayerName']['dense_2']['Precision']['result'] = 'ap_fixed<31,13>'
-# #config['LayerName']['dense_2_sigmoid']['Precision'] = 'ap_fixed<32,5>'
-# #config['LayerName']['dense_2_sigmoid']['table_size'] = '4096'
-# config['LayerName']['dense_2_sigmoid']['table_t'] = 'ap_fixed<31,13>'
-# config['LayerName']['input1']['Precision'] = 'ap_fixed<22,5>'
+#Creating HLS_model from the QKeras one using the config dict
+hls_model = hls4ml.converters.convert_from_keras_model(model, hls_config=config, output_dir=modelname +'/'+ outrootname + '/HLS_Project',fpga_part='xczu9eg-ffvb1156-2-e')
 
-
-#            SMALL NET
-
-# config['LayerName']['dense']['Precision']['weight'] = 'ap_fixed<25,9>'
-# config['LayerName']['dense']['Precision']['bias'] = 'ap_fixed<11,4>'
-# config['LayerName']['dense']['Precision']['result'] = 'ap_fixed<47,18>'
-# #config['LayerName']['dense_sigmoid']['Precision'] = 'ap_fixed<32,5>'
-# #config['LayerName']['dense_sigmoid']['table_size'] = '4096'
-# config['LayerName']['dense_sigmoid']['table_t'] = 'ap_fixed<47,18>'
-# config['LayerName']['dense_1']['Precision']['weight'] = 'ap_fixed<12,4>'
-# config['LayerName']['dense_1']['Precision']['bias'] = 'ap_fixed<7,1>'
-# config['LayerName']['dense_1']['Precision']['result'] = 'ap_fixed<33,15>'
-# #config['LayerName']['dense_1_sigmoid']['Precision'] = 'ap_fixed<32,5>'
-# #config['LayerName']['dense_1_sigmoid']['table_size'] = '4096'
-# config['LayerName']['dense_1_sigmoid']['table_t'] = 'ap_fixed<33,15>'
-# config['LayerName']['input1']['Precision'] = 'ap_fixed<22,5>'
-
-
-
-hls_model = hls4ml.converters.convert_from_keras_model(model, hls_config=config, output_dir=outrootname + '/HLS_Project',fpga_part='xqzu19eg-ffrb1517-2-i')
-#hls4ml.utils.plot_model(hls_model, show_shapes=True, show_precision=True, to_file="TEST_HLS.png")
+#Building project directory with .tcl script for Vivado_hls containing instruction for the synthesis.
 hls_model.compile()
 
-
-
-
-
-# numxtest=X_test.to_numpy()
-# print(numxtest[0])
-# print('\n')
-# print(numxtest[10])
-
-# print('\n')
-# print(numxtest[1])    
-# print('\n')
-# print(numxtest[11])
-# predictions = hls_model.predict(np.ascontiguousarray(numxtest))
-
-
-# np.savetxt("out_hls.csv",predictions,delimiter=',')
-
-#prof = hls4ml.model.profiling.numerical(keras_model=model, hls_model=hls_model)
+#Saving tracing output
 hls4ml_pred, hls4ml_trace = hls_model.trace(np.ascontiguousarray(X_test.to_numpy()))
 keras_trace = hls4ml.model.profiling.get_ymodel_keras(model, np.ascontiguousarray(X_test.to_numpy()))
 np_keras_trace = {}
 for key in keras_trace.keys():
     np_keras_trace[key] = keras_trace[key].numpy()
-check_output("if [ ! -d \"" + outrootname + "/keras_trace\" ]; then mkdir \"" + outrootname + "/keras_trace\";fi",shell=True)
-check_output("if [ ! -d \"" + outrootname + "/hls4ml_trace\" ]; then mkdir \"" + outrootname + "/hls4ml_trace\";fi",shell=True)
+check_output("if [ ! -d \"" + modelname +"/"+ outrootname + "/keras_trace\" ]; then mkdir \"" + modelname +"/"+ outrootname + "/keras_trace\";fi",shell=True)
+check_output("if [ ! -d \"" + modelname +"/"+ outrootname + "/hls4ml_trace\" ]; then mkdir \"" + modelname +"/"+ outrootname + "/hls4ml_trace\";fi",shell=True)
 for key in np_keras_trace.keys():
-    np.savetxt(outrootname + "/keras_trace/"+key+".csv",np_keras_trace[key],delimiter=',')
+    np.savetxt(modelname +"/"+outrootname + "/keras_trace/"+key+".csv",np_keras_trace[key],delimiter=',')
 for key in hls4ml_trace.keys():
-    np.savetxt(outrootname + "/hls4ml_trace/"+key+".csv",hls4ml_trace[key],delimiter=',')
+    np.savetxt(modelname +"/"+outrootname + "/hls4ml_trace/"+key+".csv",hls4ml_trace[key],delimiter=',')
--- Q_Model/QKeras_Model.py
+++ Q_Model/QKeras_Model.py
@@ -8,6 +8,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout, Activation
 from keras.optimizers import Adam
+from keras.optimizers.schedules import ExponentialDecay
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from keras.callbacks import History 
@@ -15,17 +16,18 @@ import matplotlib.pyplot as plt
 from keras.models import load_model
 from subprocess import check_output
 from qkeras.qlayers import QDense, QActivation
-from qkeras.quantizers import quantized_bits, quantized_relu
-
+from qkeras.quantizers import quantized_bits, quantized_relu,smooth_sigmoid
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"  
 
 from tensorflow_model_optimization.python.core.sparsity.keras import prune, pruning_callbacks, pruning_schedule
 from tensorflow_model_optimization.sparsity.keras import strip_pruning
-
-seed = 1563
+# 1563
+seed = 12345
 np.random.seed(seed)
 set_seed(seed)
 
-outrootname = "QKeras_Model_4_Pruned_120"
+outrootname = "QModel"
 Nfolder = check_output("if [ ! -d \"" + outrootname + "\" ]; then mkdir \"" + outrootname + "\";fi",shell=True)
 Nfolder = check_output("if [ ! -d \"" + outrootname + "/hist\" ]; then mkdir \"" + outrootname + "/hist\";fi",shell=True)
 
@@ -91,6 +93,15 @@ def preprocess_targets(muon_dataframe):
 
 
 def datasets():
+    '''
+    Normalization of phiBending and cutting entries with a pT higher than 200 GeV
+
+    Returns
+    -------
+    int
+        No error code.
+
+    '''
     out_dataframe = pd.read_csv('../output/bxcut_full_muon.csv')
     muon_dataframe_test = pd.read_csv('../output/bxcut_full_test.csv')
     
@@ -104,48 +115,58 @@ def datasets():
     muon_dataframe_test["3dtPrimitive.phiB"] = muon_dataframe_test["3dtPrimitive.phiB"]/512.
     muon_dataframe_test["4dtPrimitive.phiB"] = muon_dataframe_test["4dtPrimitive.phiB"]/512.
     
-    out_dataframe = out_dataframe[out_dataframe['genParticle.pt'] <= 200]
+    
     
     #Creating different datasets
     
-    out_dataframe60 = out_dataframe[out_dataframe['genParticle.pt'] <= 60]
-    out_dataframe120 = out_dataframe[out_dataframe['genParticle.pt'] <= 120]
-    out_dataframe120 = out_dataframe120[out_dataframe120['genParticle.pt'] > 60]
-    out_dataframe180 = out_dataframe[out_dataframe['genParticle.pt'] <= 180]
-    out_dataframe180 = out_dataframe180[out_dataframe180['genParticle.pt'] > 120]
+    # out_dataframe60 = out_dataframe[out_dataframe['genParticle.pt'] <= 60]
+    # out_dataframe120 = out_dataframe[out_dataframe['genParticle.pt'] <= 120]
+    # out_dataframe120 = out_dataframe120[out_dataframe120['genParticle.pt'] > 60]
+    # out_dataframe180 = out_dataframe[out_dataframe['genParticle.pt'] <= 180]
+    # out_dataframe180 = out_dataframe180[out_dataframe180['genParticle.pt'] > 120]
+       
+    # out_dataframe60.to_csv("out60.csv")
+    # out_dataframe120.to_csv("out120.csv")
+    # out_dataframe180.to_csv("out180.csv")
     
-
+    # muon_dataframe_test60 = muon_dataframe_test[muon_dataframe_test['genParticle.pt'] <= 60]
+    # muon_dataframe_test120 = muon_dataframe_test[muon_dataframe_test['genParticle.pt'] <= 120]
+    # muon_dataframe_test120 = muon_dataframe_test120[muon_dataframe_test120['genParticle.pt'] > 60]
+    # muon_dataframe_test180 = muon_dataframe_test[muon_dataframe_test['genParticle.pt'] <= 180]
+    # muon_dataframe_test180 = muon_dataframe_test180[muon_dataframe_test180['genParticle.pt'] > 120]
+    
+    # muon_dataframe_test60.to_csv("test60.csv")
+    # muon_dataframe_test120.to_csv("test120.csv")
+    # muon_dataframe_test180.to_csv("test180.csv")
     
     
     
-    out_dataframe60.to_csv("out60.csv")
-    out_dataframe120.to_csv("out120.csv")
-    out_dataframe180.to_csv("out180.csv")
     
-    
-    out_dataframe.to_csv("out_data.csv")
-    
+    out_dataframe = out_dataframe[out_dataframe['genParticle.pt'] <= 200]
     muon_dataframe_test = muon_dataframe_test[muon_dataframe_test['genParticle.pt'] <= 200]
-    
-    muon_dataframe_test60 = muon_dataframe_test[muon_dataframe_test['genParticle.pt'] <= 60]
-    muon_dataframe_test120 = muon_dataframe_test[muon_dataframe_test['genParticle.pt'] <= 120]
-    muon_dataframe_test120 = muon_dataframe_test120[muon_dataframe_test120['genParticle.pt'] > 60]
-    muon_dataframe_test180 = muon_dataframe_test[muon_dataframe_test['genParticle.pt'] <= 180]
-    muon_dataframe_test180 = muon_dataframe_test180[muon_dataframe_test180['genParticle.pt'] > 120]
-    
-    muon_dataframe_test60.to_csv("test60.csv")
-    muon_dataframe_test120.to_csv("test120.csv")
-    muon_dataframe_test180.to_csv("test180.csv")
-    
-    
-    muon_dataframe_test.to_csv("muon_test.csv")
-
+    # muon_dataframe_test.to_csv("muon_test.csv")
+    # out_dataframe.to_csv("out_data.csv")
     return 0
 
 def preproc(test=False):
-    
-    out_dataframe = pd.read_csv('out120.csv')
-    muon_dataframe_test = pd.read_csv('test120.csv')
+    '''
+    Selection of features used in Training/Test. Changing the quality feature to a binary one.
+
+    Parameters
+    ----------
+    test : Boolean, optional
+        Flag to preprocess the training set (False) or the testing set (True). The default is False.
+
+    Returns
+    -------
+    List of Numpy Arrays
+        if test=True: numpy arrays containing input data and true labels respectively for testing.
+    List of Numpy Arrays
+        if test=False: numpy arrays containing input data and true labels respectively for testing.
+
+    '''
+    out_dataframe = pd.read_csv('out_data.csv')
+    muon_dataframe_test = pd.read_csv('muon_test.csv')
     
     X = preprocess_features(out_dataframe)
     X_test = preprocess_features(muon_dataframe_test)
@@ -185,23 +206,63 @@ def preproc(test=False):
 
 
 def Q_baseline_model(size,epochs,optimizer,X_training,y_training,X_validation,y_validation,output_name):
-    
+    '''
+    NN Model constructor with loss and accuracy plots.
+
+    Parameters
+    ----------
+    size : int
+        Batch size used in the training process.
+    epochs : int
+        Number of epochs the model will be trained.
+    optimizer : keras.optimizer
+        Optimizer function.
+    X_training : Numpy array
+        Training data set.
+    y_training : Numpy array
+        True labels for the training set.
+    X_validation : Numpy array
+        Validation data set.
+    y_validation : Numpy array
+        True labels for the validation set.
+    output_name : str
+        Name used for saved plots.
+
+    Returns
+    -------
+    model : qkeras.sequential
+        QKeras model.
+    w : numpy array
+        Array of final weights used in the model for later inference.
+
+    '''
     pruning = True
     # create model
     name="RMSE validation"
     name2="RMSE training"
     history = History()
     model = Sequential()
-    model.add(QDense(50,  input_shape=(27,),kernel_quantizer=quantized_bits(16,0),bias_quantizer=quantized_bits(16,0), kernel_initializer='random_normal'))
-    model.add(QActivation(activation=quantized_relu(16), name='relu1'))
+    model.add(QDense(60,  input_shape=(27,),kernel_quantizer=quantized_bits(16,1),bias_quantizer=quantized_bits(16,1), kernel_initializer='random_normal'))
+    model.add(QActivation(activation=quantized_relu(16,1), name='relu1'))
+    model.add(QDense(50,kernel_quantizer=quantized_bits(16,1),bias_quantizer=quantized_bits(16,1)))
+    model.add(QActivation(activation=quantized_relu(16,1), name='relu2'))
     # model.add(Dropout(rate=0.2))
-    model.add(QDense(25,kernel_quantizer=quantized_bits(16,0),bias_quantizer=quantized_bits(16,0)))
-    model.add(QActivation(activation=quantized_relu(16), name='relu2'))
-    model.add(QDense(10,kernel_quantizer=quantized_bits(16,0),bias_quantizer=quantized_bits(16,0)))
-    model.add(QActivation(activation=quantized_relu(16), name='relu3'))
-    # model.add(Dropout(rate=0.2))
-    model.add(Dense(1,activation='sigmoid'))
-    #model.add(QActivation(activation=hard_sigmoid(6), name='sigm3'))
+    model.add(QDense(30,kernel_quantizer=quantized_bits(16,1),bias_quantizer=quantized_bits(16,1)))
+    model.add(QActivation(activation=quantized_relu(16,1), name='relu3'))
+    model.add(QDense(40,kernel_quantizer=quantized_bits(16,1),bias_quantizer=quantized_bits(16,1)))
+    model.add(QActivation(activation=quantized_relu(16,1), name='relu4'))
+    model.add(QDense(15,kernel_quantizer=quantized_bits(16,1),bias_quantizer=quantized_bits(16,1)))
+    model.add(QActivation(activation=quantized_relu(16,1), name='relu5'))
+    
+    # model.add(QDense(50,  input_shape=(27,),kernel_quantizer=quantized_bits(16,1),bias_quantizer=quantized_bits(16,1), kernel_initializer='random_normal'))
+    # model.add(QActivation(activation=quantized_relu(16,1), name='relu1'))
+    # model.add(QDense(25,kernel_quantizer=quantized_bits(16,1),bias_quantizer=quantized_bits(16,1)))
+    # model.add(QActivation(activation=quantized_relu(16,1), name='relu2'))
+    # model.add(QDense(10,kernel_quantizer=quantized_bits(16,1),bias_quantizer=quantized_bits(16,1)))
+    # model.add(QActivation(activation=quantized_relu(16,1), name='relu3'))   
+    # # model.add(Dropout(rate=0.2))
+    model.add(QDense(1,kernel_quantizer=quantized_bits(16,1)))
+    model.add(QActivation(activation=quantized_relu(16,1), name='relu6'))
     #model.add(Activation("sigmoid"))
     
     if pruning == True:
@@ -313,17 +374,41 @@ X,Y = preproc()
 
 
 def run(X,Y):
+    '''
+    Main Function
+
+    Parameters
+    ----------
+    X : Numpy array
+        Data used to train the model. 20% of this dataset will be used for validation.
+    Y : TYPE
+        True label associated to the provided data.
+
+    Returns
+    -------
+    model : qkeras.sequential
+        QKeras model.
+    w : numpy array
+        Array of final weights used in the model for later inference.
+
+    '''
     x_train, x_valid, y_train, y_valid = train_test_split(X, Y, test_size=0.2,random_state=seed)
-    adam = Adam(lr=0.0001) #Default 0.001
-    model,w = Q_baseline_model(100,50,adam,x_train, y_train, x_valid, y_valid,'Adam.png')
-    model.save(outrootname + '/' + outrootname + '.h5')
+    initial_lr = 0.001
+    #learning_schedule = ExponentialDecay(initial_lr,decay_steps=10000,decay_rate=0.96,staircase=True)
+    adam = Adam(learning_rate=initial_lr) #Default 0.001
+    model,w = Q_baseline_model(300,130,adam,x_train, y_train, x_valid, y_valid,'Adam.png')
+    model.save(outrootname + '/' + outrootname + '.h5',)
     np.savetxt(outrootname + '/' +"hist/weights_1.csv",w[0][0],delimiter=',')
     np.savetxt(outrootname + '/' +"hist/weights_2.csv",w[2][0],delimiter=',')
     np.savetxt(outrootname + '/' +"hist/weights_3.csv",w[4][0],delimiter=',')
-    np.savetxt(outrootname + '/' +"hist/weights_3.csv",w[6][0],delimiter=',')
+    np.savetxt(outrootname + '/' +"hist/weights_4.csv",w[6][0],delimiter=',')
+    np.savetxt(outrootname + '/' +"hist/weights_5.csv",w[8][0],delimiter=',')
+    np.savetxt(outrootname + '/' +"hist/weights_6.csv",w[10][0],delimiter=',')
     np.savetxt(outrootname + '/' +"hist/bias_1.csv",w[0][1],delimiter=',')
     np.savetxt(outrootname + '/' +"hist/bias_2.csv",w[2][1],delimiter=',')
     np.savetxt(outrootname + '/' +"hist/bias_3.csv",w[4][1],delimiter=',')
-    np.savetxt(outrootname + '/' +"hist/bias_3.csv",w[6][1],delimiter=',')
+    np.savetxt(outrootname + '/' +"hist/bias_4.csv",w[6][1],delimiter=',')
+    np.savetxt(outrootname + '/' +"hist/bias_5.csv",w[8][1],delimiter=',')
+    np.savetxt(outrootname + '/' +"hist/bias_6.csv",w[10][1],delimiter=',')
     return model,w
 
